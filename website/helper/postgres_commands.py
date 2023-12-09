59e5ee8382dd047f676af2db8aa2bd9bfbfcf2d7
AMarinjo <ammaar_1999@hotmail.com>
2023-12-09 15:53:23 -0600

    Committing most recent changes to the project. Adds search funtionality and profiles for particular songs

--- website/helper/postgres_commands.py
+++ website/helper/postgres_commands.py
@@ -42,6 +42,22 @@ def create_table(table_name):
     return table_schema
 
 
+def update_artists(table_name):
+    """Simple method that updates the artists column and changes the semicolumns
+    into commas within the string
+
+    Args:
+        table_name (str): Table name used within database
+    """
+
+    update_query = f"""
+    UPDATE {table_name}
+    SET artists = REPLACE(artists, ';', ', ')
+    """
+
+    return update_query
+
+
 def check_table_available(table_name):
     """Simple method to check whether or not the table being added is available or not
 
@@ -55,3 +71,73 @@ def check_table_available(table_name):
     """
 
     return table_available
+
+
+def search_query(table_name):
+    """Simple method to search database
+
+    Args:
+        table_name (str): Table name used within database
+    """
+
+    search = f"""
+        SELECT track_id, track_name || ' - ' || artists AS track_and_artists
+        FROM {table_name} 
+        WHERE track_name ILIKE %s OR artists ILIKE %s
+        ORDER BY 
+            similarity(track_name, %s) + similarity(artists, %s) DESC
+        LIMIT 20
+    """
+
+    return search
+
+
+def find_list_query(table_name):
+    """Simple method to search database
+
+    Args:
+        table_name (str): Table name used within database
+    """
+
+    find_list = f"""
+        SELECT track_id, track_name || ' - ' || artists AS track_and_artists, album_name, track_genre 
+        FROM {table_name} 
+        WHERE track_name ILIKE %s OR artists ILIKE %s
+        ORDER BY 
+            similarity(track_name, %s) + similarity(artists, %s) DESC
+        LIMIT 50
+    """
+
+    return find_list
+
+
+def find_by_id(table_name, search_id):
+    """Simple method to search database by id
+
+    Args:
+        table_name (str): Table name used within database
+        search_id (str): Id that needs to be searched by
+    """
+
+    search_query = f"""
+        SELECT track_id, track_name, artists, album_name, track_genre 
+        FROM {table_name} 
+        WHERE track_id = '{search_id}'
+    """
+
+    return search_query
+
+
+def tempo(table_name, danceability, liveness):
+    """Find specific type of tempo and songs
+
+    Args:
+        table_name (str): Table name used within database
+        danceability (int): Danceability one might be looking for
+        liveness (int): Liveness one might need to be looking for
+    """
+
+    tempo = f"SELECT track_name FROM {table_name} ORDER BY ABS(danceability"
+    tempo += f" {danceability}) + ABS(liveness-{liveness}) LIMIT 5;"
+
+    return tempo
--- website/views.py
+++ website/views.py
@@ -19,22 +19,55 @@ def homepage():
     return render_template("home.html")
 
 
-@views.route("/search")
+@views.route("/search", methods=["GET", "POST"])
 def search():
     """Used for searching the database. Searches postgres database specifically
 
     Returns:
         Render of the search template
     """
+    if request.method == "POST":
+        query = request.form.get("query")
+
+        if query:
+            postgres = PostgresModel()
+            results = postgres.find_list(query)
+            postgres.close()
+            return render_template("find.html", results=results, query=query)
+
+        return render_template("home.html")
+
     query = request.args.get("query")
 
     if query:
         postgres = PostgresModel()
-
         results = postgres.search(query)
-
         postgres.close()
 
-        return render_template("home.html", results=results, query=query)
-    else:
-        return render_template("home.html", message="Please enter a search query.")
+        suggestions = [
+            {
+                "track_id": result[0],
+                "track_name": result[1],
+                "redirect_url": f"/redirect/{result[0]}",
+            }
+            for result in results
+        ]
+        return jsonify({"suggestions": suggestions})
+
+
+@views.route("/redirect/<suggestion>")
+def redirect_page(suggestion):
+    """Render a page based on the selected suggestion.
+
+    Args:
+        suggestion (str): The selected suggestion.
+    Returns:
+        Render of a template specific to the selected suggestion.
+    """
+    postgres = PostgresModel()
+    results_post = postgres.find_by_id(suggestion)
+    postgres.close()
+    print(results_post)
+    results_neo = ""
+
+    return render_template(f"profile.html", results_post=results_post[0])
